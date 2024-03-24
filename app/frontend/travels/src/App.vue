783ae72e2a149acef38f976dd497d47b44ad7362
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-03-24 15:14:57 -0300

    feat: add layout

--- app/frontend/travels/src/App.vue
+++ app/frontend/travels/src/App.vue
@@ -1,8 +1,32 @@
+
 <template>
-  <nav>
-  </nav>
-  <router-view/>
+  <div class="page">
+    <AppSidebarVue/>
+    <router-view>
+    </router-view>
+
+  </div>
+  
 </template>
 
+
+<script>
+import AppSidebarVue from "./components/AppSidebar.vue";
+
+export default {
+
+  components: {
+    AppSidebarVue
+  }
+}
+</script>
+
 <style>
+  .page{
+    display: flex;
+    min-height: 100vh;
+    overflow: hidden;
+    position: absolute;
+    width: 100%;
+  }
 </style>
