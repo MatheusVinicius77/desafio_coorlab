cb3ff008947efc1db8cfaa660c82b362c1360c2b
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-03-24 10:55:09 -0300

    feat: add endpoint for most comfortable

--- app/backend/controller.py
+++ app/backend/controller.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from model import Transport, TransportDB
-from utils import conver_string_to_date
+from utils import conver_string_to_date, extract_hours
 import os
 
 db_file_path =  os.path.join(os.path.dirname(os.getcwd()), 'data.json')
@@ -13,12 +13,20 @@ def read_transports():
     return db.transports
 
 
-@router.get("/transports/{destination}/{date}")
+@router.get("/transports/comfortable/{destination}/{date}")
 def read_transports(destination:str, date:str):
+    # GET ALL TRANSPORTS FOR THAT DESTINATION
     matching_destination_transports = [transport for transport in db.transports if transport.city.lower() == destination.lower()]
     travel_date = conver_string_to_date(date)
+    
 
-    print(matching_destination_transports)
     if matching_destination_transports:
-        return matching_destination_transports
+        # SORTING ALL THE TRANSPORTS BY ITS DURATION
+        sorted_transports = sorted(matching_destination_transports, key= extract_hours)
+        
+        # GETTING THE MOST COMFORTABLE 
+        more_comfortable = sorted_transports[0]
+        
+        return more_comfortable
+    
     raise HTTPException(status_code=404, detail=f"No transports found for destination: {destination}")
